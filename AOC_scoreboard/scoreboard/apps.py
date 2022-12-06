ef19eb4aa65c1d116516629fbedb4b109d435203
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-12-06 19:00:29 +0100

    fixed issue where updater would start in dev mode

--- AOC_scoreboard/scoreboard/apps.py
+++ AOC_scoreboard/scoreboard/apps.py
@@ -1,9 +1,14 @@
 from django.apps import AppConfig
+from django.conf import settings
+import socket
 
 class ScoreboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scoreboard'
     
     def ready(self):
-        from scoreboard_updater import updater
-        updater.start()
+        if settings.DEBUG:
+            print(f"Server launched on {socket.gethostbyname(socket.gethostname())} and Debug mode is on\nReocurring scoreboard update will not be executed!")
+        else:
+            from scoreboard_updater import updater
+            updater.start()
